1f5b9f1b1f04c96e22cf9205bce106242b7ec6da
Robert <roberto.luna2021@gmail.com>
2024-02-01 06:12:53 -0600

    Completely refactored scraping logic. Setup PostgreSQL data push

--- backend/scraper/index.js
+++ backend/scraper/index.js
@@ -1,202 +1,211 @@
 require("dotenv").config();
+const puppeteer = require("puppeteer");
 const { Cluster } = require("puppeteer-cluster");
 const fs = require("fs/promises");
 const path = require("path");
+const { Client } = require("pg");
+const { random } = require("user-agents");
 
-/**
- * Extracts the category from a given link.
- *
- * @param {string} link - The link to extract the category from.
- * @return {string} The category extracted from the link.
- */
-function getCategoryFromLink(link) {
-  const { categoriesMapping } = require("./categories.js");
+function logErrorToFile(message) {
+  const logFilePath = path.join(__dirname, "logs", "scraping_error.log");
+  const timestamp = new Date().toISOString();
+  const logMessage = `${timestamp}: - ${message}\n`;
 
-  for (const category in categoriesMapping) {
-    if (categoriesMapping[category].some((pattern) => link.includes(pattern))) {
-      return category;
+  // Append the error message to the log file
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error("Error writing to log file:", err);
+    }
+  });
+}
+
+function getStoreNameFromLink(link) {
+  const { stores } = require("./links.js");
+  for (const [storeName, { url }] of Object.entries(stores)) {
+    if (link.startsWith(url)) {
+      return storeName;
     }
   }
-  // Default to "Other" category if no match is found
   return "Other";
 }
 
-/**
- * Scrapes the names of items from a web page.
- *
- * @param {Page} page - The page to scrape the item names from.
- * @return {Promise<Array<string>>} An array of item names.
- */
-async function scrapeItemNames(page) {
-  return await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".e-vijstc")).map((x) => {
-      const item = x.innerText
-        .split(",")[0]
-        .replace(/\s*,?\s*-count$/i, "")
-        .trim();
-      return item;
-    });
-  });
-}
-
-/**
- * Scrapes the units from the given page.
- *
- * @param {Object} page - The page object to scrape units from.
- * @return {Promise<Array>} An array of unit text values.
- */
-async function scrapeUnits(page) {
-  return await page.evaluate(() => {
-    const elementHeader = document.querySelectorAll(".e-fsno8i");
-    const unitTextArray = [];
-
-    elementHeader.forEach((element) => {
-      const unitValue = element.querySelectorAll(".e-wfknno");
-      if (unitValue.length > 0) {
-        unitValue.forEach((unit) => {
-          unitTextArray.push(unit.innerText);
-        });
-      } else {
-        unitTextArray.push(null); // Push null if .e-wfknno is not found
+function getCategoryFromLink(link) {
+  const { stores } = require("./links.js");
+  for (const [, { url, categories }] of Object.entries(stores)) {
+    if (link.startsWith(url)) {
+      for (const [categoryName, subpaths] of Object.entries(categories)) {
+        if (subpaths.some((subpath) => link.includes(`${url}/${subpath}`))) {
+          return categoryName;
+        }
       }
-    });
-    return unitTextArray;
-  });
+    }
+  }
+  return "Other";
 }
 
-/**
- * Scrapes images from a web page.
- *
- * @param {Page} page - The page to scrape images from.
- * @return {Promise<Array<string>>} An array of URLs of the scraped images.
- */
-async function scrapeImages(page) {
-  return await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".e-ec1gba img"))
-      .map((x) => {
-        const srcset = x.getAttribute("srcset");
-        if (srcset) {
-          const srcsetParts = srcset.split(/,\s+/);
-          const firstURL = srcsetParts[0].split(" ")[0];
-          return firstURL || null;
-        } else {
-          return null;
-        }
-      })
-      .filter((image) => image !== null);
-  });
-}
+async function scrapeProductInfo(page) {
+  const selectors = {
+    productContainer: ".e-fsno8i",
+    productName: ".e-n9fc4b",
+    productPrice: ".e-m67vuy",
+    productUnits: ".e-zjik7",
+    productUnitElements: ".e-k9ly30", // Child of .e-zjik7
+    productImage: ".e-ec1gba img",
+  };
 
-/**
- * Scrapes prices from a web page.
- *
- * @param {Page} page - The page to scrape prices from.
- * @return {Promise<Array<string>>} An array of prices
- */
-async function scrapePrices(page) {
-  return await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".e-1jioxed"))
-      .map((x) => {
-        const price = x.innerText.split("\n")[0];
+  return await page.evaluate(
+    ({ selectors }) => {
+      const extractProductInfo = (element) => {
+        // Grab product name
+        const productNameContainer = element.querySelector(
+          selectors.productName
+        ).innerText;
+
+        // Grab product price
+        const priceElementContainer = element.querySelector(
+          selectors.productPrice
+        );
+        const productText = priceElementContainer.innerText;
+        const unitMatch = /\/lb|\/pkg|\/?each/.exec(productText);
         let unit = "";
-        if (x.innerText.includes("/lb")) {
-          unit = "/lb";
-        } else if (x.innerText.includes("pkg")) {
-          unit = "/pkg";
-        } else if (x.innerText.includes("each")) {
-          unit = "/each";
+
+        if (unitMatch) {
+          if (unitMatch[0].startsWith("/")) {
+            unit = unitMatch[0];
+          } else {
+            unit = " " + unitMatch[0];
+          }
         }
-        return price + unit;
-      })
-      .filter((x) => x.includes("$"));
-  });
+        const productPrice = productText.split("\n")[0] + unit || null;
+
+        // Grab product units
+        const productUnitContainer = element.querySelector(
+          selectors.productUnits
+        );
+        const unitElements = productUnitContainer
+          ? Array.from(
+              productUnitContainer.querySelectorAll(
+                selectors.productUnitElements
+              )
+            ).map((unitElement) => unitElement.innerText)
+          : null;
+        const productUnits =
+          unitElements && unitElements.length === 1
+            ? unitElements[0]
+            : unitElements;
+
+        // Grab product image
+        const productImageContainer = element.querySelector(
+          selectors.productImage
+        );
+        const productImage =
+          productImageContainer
+            .getAttribute("srcset")
+            .split(/,\s+/)[0]
+            .split(" ")[0] || null;
+
+        // Create a productUniqueID
+        const productUniqueID = productNameContainer + productPrice;
+
+        // Return data
+        return {
+          productName: productNameContainer,
+          productPrice: productPrice,
+          productUnits: productUnits,
+          productImage: productImage,
+          productUniqueID: productUniqueID,
+        };
+      };
+
+      return Array.from(
+        document.querySelectorAll(selectors.productContainer)
+      ).map(extractProductInfo);
+    },
+    { selectors }
+  );
 }
 
-/**
- * Scrapes data from multiple stores using a list of links.
- *
- * @param {Array<string>} links - The list of links to the stores.
- * @param {string} storeName - The name of the store.
- * @return {Promise<void>} - A promise that resolves when the scraping is complete.
- */
-async function scrapeStores(links, storeName) {
+async function grabCookies() {
+  const cookiesFileExists = await fs
+    .access("./data/cookies.json")
+    .then(() => true)
+    .catch(() => false);
+
+  if (!cookiesFileExists) {
+    const browser = await puppeteer.launch({
+      headless: false,
+      userDataDir: userDataDir,
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.instacart.com/", { waitUntil: "load" });
+
+    // Save Cookies
+    const cookies = await page.cookies();
+    await fs.writeFile("./data/cookies.json", JSON.stringify(cookies, null, 2));
+
+    await browser.close();
+  }
+}
+
+async function scrapeStores(links) {
+  let errorOccurred = false; // flag to keep track of errors
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
     maxConcurrency: 4, // Adjust this based on your system's capacity
     timeout: 2147483647, // Set timeout to safest 32 bit integer as puppeteer-cluster does not support max_timeout
     monitor: true,
     puppeteerOptions: {
-      headless: "new",
-      defaultViewport: false,
+      headless: false,
     },
   });
 
-  // Event handler to be called in case of problems
+  // Event handle to be called in case of problems
   cluster.on("taskerror", (err, data) => {
-    console.log(`Error crawling ${data}: ${err.message}`);
+    const errorMessage = `Error crawling ${data}: ${err.message}`;
+    logErrorToFile(errorMessage);
+    errorOccurred = true;
   });
+
+  let allData = [];
 
   await cluster.task(async ({ page, data: storeLink }) => {
     // Set Sleep Function
     const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+    const buttonSelector =
+      "#store-wrapper > div > div > div.e-1bfgc1k > div.e-14cjhfa > button";
+
+    // Set Cookies
+    const cookiesString = await fs.readFile("./data/cookies.json");
+    const cookies = JSON.parse(cookiesString);
+    await page.setCookie(...cookies);
+
+    // Set User Agent
+    const randomUserAgent = random();
+    await page.setUserAgent(randomUserAgent.toString());
 
     await page.goto(storeLink);
-    await sleep(5000);
+    const productCategory = getCategoryFromLink(storeLink);
+    const productStoreName = getStoreNameFromLink(storeLink);
+
+    await page.waitForSelector(buttonSelector, {
+      visible: true,
+    });
     while (true) {
-      const loadMoreButton = await page.$(
-        "#store-wrapper > div > div > div.e-1bfgc1k > div.e-14cjhfa > button"
-      );
+      const loadMoreButton = await page.$(buttonSelector);
       if (!loadMoreButton) {
         break;
       }
       await loadMoreButton.click();
-      await sleep(2500);
+      await sleep(1500);
     }
 
-    const itemName = await scrapeItemNames(page);
-    const prices = await scrapePrices(page);
-    const img = await scrapeImages(page);
-    const category = getCategoryFromLink(storeLink);
-    const unit = await scrapeUnits(page);
-
-    const combinedData = itemName.map((item, index) => ({
-      category: category, // Get the category from the link
-      item: item,
-      price: prices[index],
-      image: img[index],
-      unit: unit[index],
-    }));
-
-    let existingData = {};
-    const dataFilePath = path.join(__dirname, "../../data/storeData.json");
-    try {
-      const existingDataStr = await fs.readFile(dataFilePath, "utf-8");
-      existingData = JSON.parse(existingDataStr);
-    } catch (error) {}
-
-    if (!existingData[storeName]) {
-      existingData[storeName] = [];
-    }
-
-    const uniqueData = combinedData.filter((newItem) => {
-      // Check for duplicates based on the 'item' property
-      const isDuplicate = existingData[storeName].some(
-        (existingItem) => existingItem.item === newItem.item
-      );
-
-      // Check if the category matches
-      const categoryMatches = existingData[storeName].every(
-        (existingItem) => existingItem.category === newItem.category
-      );
-
-      // Only add the item if it's not a duplicate and the category matches
-      return !isDuplicate && categoryMatches;
+    const data = await scrapeProductInfo(page);
+    // Append productCategory data and handle duplicate names
+    data.forEach((product) => {
+      product.productCategory = productCategory;
+      product.productStoreName = productStoreName;
     });
-
-    existingData[storeName].push(...uniqueData);
-
-    await fs.writeFile(dataFilePath, JSON.stringify(existingData, null, 2));
-    console.log("Scraped data from:", storeLink);
+    allData.push(...data);
   });
 
   for (const link of links) {
@@ -205,21 +214,73 @@ async function scrapeStores(links, storeName) {
 
   await cluster.idle();
   await cluster.close();
+
+  if (errorOccurred) {
+    throw new Error("An error occurred during scraping"); // throw if there was an error
+  }
+
+  return allData;
 }
 
-/**
- * Executes the main function.
- *
- * @return {Promise<void>} A promise that resolves when the function is finished executing.
- */
+async function pushPostgresDB(data) {
+  const pgClient = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+  });
+
+  try {
+    await pgClient.connect();
+
+    for (const product of data) {
+      const updateQuery = `
+        INSERT INTO products (product_unique_id, product_name, product_price, product_units, product_image, product_category, product_store_name)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        ON CONFLICT (product_unique_id) DO UPDATE
+        SET
+          product_name = $2,
+          product_price = $3,
+          product_units = $4,
+          product_image = $5,
+          product_category = $6,
+          product_store_name = $7
+      `;
+
+      const values = [
+        product.productUniqueID,
+        product.productName,
+        product.productPrice,
+        product.productUnits,
+        product.productImage,
+        product.productCategory,
+        product.productStoreName,
+      ];
+
+      await pgClient.query(updateQuery, values);
+    }
+  } catch (err) {
+    console.error("Error connecting to PostgreSQL:", err);
+  } finally {
+    await pgClient.end();
+    console.log("Data has been pushed to PostgreSQL");
+  }
+}
+
 async function main() {
-  const { hebLinks, costcoLinks } = require("./links.js"); // Import the links from your module
-  await scrapeStores(hebLinks, "heb");
-  // await scrapeStores(costcoLinks, "costco");
+  try {
+    await grabCookies();
+    const { allLinks } = require("./links.js"); // Import the links from your module
+    const allData = await scrapeStores(allLinks);
+    await pushPostgresDB(allData);
+  } catch (err) {
+    const errorMessage = `Scraping failed, not pushing to PostgreSQL: ${err}`;
+    logErrorToFile(errorMessage);
+    console.error(
+      "An error has occured during scraping, please check the logs for more details. The log file is located at ./logs/scraping_error.log"
+    );
+  }
 }
 
 main();
-
-// TODO::
-// Add puppeteer stealth plugin
-// Connect to local database
---
+++ sql/basic_queries.sql
@@ -0,0 +1,21 @@
+-- Grab Basic Data 
+SELECT product_unique_id, product_name, product_price, product_store_name 
+FROM products 
+
+-- Check for Duplicate values
+SELECT product_name, product_store_name, COUNT(*) 
+FROM products 
+GROUP BY product_name, product_store_name 
+HAVING COUNT(*) > 1
+
+-- Grab specific string data
+SELECT product_name, product_units, product_price
+FROM products
+WHERE product_name LIKE '%Watermelon%'
+
+-- Grab all data
+SELECT * FROM products
+ORDER BY product_unique_id ASC
+
+-- Grab size of table
+SELECT pg_size_pretty(pg_relation_size('products'));
